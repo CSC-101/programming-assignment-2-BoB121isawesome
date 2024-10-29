03f024a0ddd1e5f1a84071c6b60159bea58fab49
BoB121isawesome <jaydonkchen@gmail.com>
2024-10-28 22:10:13 -0700

    Programming Assignment 2 submission

--- hw2.py
+++ hw2.py
@@ -1,20 +1,75 @@
 import data
+from data import Point, Rectangle
 
 # Write your functions for each part in the space below.
 
 # Part 1
-
+def create_rectangle(point1, point2):
+    top_left= Point(min(point1.x,point2.x),max(point1.y,point2.y))
+    bottom_right= Point(max(point1.x,point2.x),min(point1.y,point2.y))
+    return Rectangle(top_left,bottom_right)
 
 # Part 2
-
+def shorter_duration_than(dur1,dur2):
+    if dur1.minutes != dur2.minutes:
+        return dur1.minutes < dur2.minutes
+    return dur1.seconds < dur2.seconds
 
 # Part 3
-
+def songs_shorter_than(song_list,dur):
+    short_songs=[]
+    for i in range(len(song_list)):
+        if shorter_duration_than(song_list[i].duration,dur) == True:
+            short_songs.append(song_list[i])
+    return short_songs
 
 # Part 4
-
+def running_time(songs_list,num_list):
+    run_time=data.Duration(0,0)
+    for song in num_list:
+        run_time.minutes=run_time.minutes+songs_list[song].duration.minutes
+        run_time.seconds=run_time.seconds+songs_list[song].duration.seconds
+    while run_time.seconds>=60:
+        run_time.seconds=run_time.seconds-60
+        run_time.minutes=run_time.minutes+1
+    return run_time
 
 # Part 5
-
+def validate_route(routes:list[list],route:list):
+    valid_routes=len(route)-1
+    for i in range(len(routes)-2):
+        city1 = route[i]
+        city2 = route[i+1]
+        if ([city1,city2] not in routes) and ([city2,city1] not in routes):
+            valid_routes==valid_routes
+        else:
+            valid_routes=valid_routes-1
+    if valid_routes==0:
+        return True
+    else:
+        return False
 
 # Part 6
+def largest(some_list:[[int]]):
+    large=0
+    for i in range(len(some_list)):
+        if some_list[large][0] <some_list[i][0]:
+            large=i
+    return large
+
+
+def longest_repetition(num_list):
+    if len(num_list)==0 or num_list == []:
+        return None
+    counter=1
+    index=0
+    counts_array=[]
+    for i in range(len(num_list)-1):
+        if num_list[i]==num_list[i+1]:
+            counter=counter+1
+        else:
+            counts_array.append([counter,index])
+            index= i + 1
+            counter=1
+    counts_array.append([counter, index])
+    return counts_array[largest(counts_array)][1]
--- hw2_tests.py
+++ hw2_tests.py
@@ -7,25 +7,98 @@ import unittest
 
 class TestCases(unittest.TestCase):
     # Part 1
+    def test_create_rectangle(self):
+        point1= data.Point(1, 1)
+        point2= data.Point(3, 2)
+        expected= data.Rectangle(data.Point(1,2), data.Point(3,1))
+        self.assertEqual(expected, hw2.create_rectangle(point1, point2))
 
+    def test_create_rectangle_2(self):
+        point1= data.Point(-1, -6)
+        point2= data.Point(5, -6)
+        expected= data.Rectangle(data.Point(-1,-6), data.Point(5,-6))
+        self.assertEqual(expected, hw2.create_rectangle(point1, point2))
 
     # Part 2
+    def test_shorter_duration_than(self):
+        dur1 = data.Duration(3,52)
+        dur2 = data.Duration(4,21)
+        self.assertEqual(True, hw2.shorter_duration_than(dur1,dur2))
 
+    def test_shorter_duration_than_2(self):
+        dur1 = data.Duration(3,52)
+        dur2 = data.Duration(3,52)
+        self.assertEqual(False, hw2.shorter_duration_than(dur1,dur2))
 
     # Part 3
+    def test_songs_shorter_than(self):
+        dur = data.Duration(3,52)
+        song1 = data.Song('Kanye','Runaway',data.Duration(3,51))
+        song2 = data.Song('Travis Scott','FEIN',data.Duration(4,51))
+        song3 = data.Song('SZA','Kill Bill',data.Duration(3,31))
+        song4 = data.Song('NewJeans','OMG',data.Duration(4,50))
+        song_list=[song1,song2,song3,song4]
+        expected=[song1,song3]
+        self.assertEqual(expected, hw2.songs_shorter_than(song_list,dur))
 
+    def test_songs_shorter_than_2(self):
+        dur = data.Duration(4,39)
+        song1 = data.Song('Joji','Die For You',data.Duration(3,51))
+        song2 = data.Song('NewJeans', 'Ditto', data.Duration(3, 31))
+        song3 = data.Song('Travis Scott','FEIN',data.Duration(4,51))
+        song4 = data.Song('Childish Gambino','Les',data.Duration(5,17))
+        song_list=[song1,song2,song3,song4]
+        expected=[song1,song2]
+        self.assertEqual(expected, hw2.songs_shorter_than(song_list,dur))
 
     # Part 4
+    def test_running_time(self):
+        song1 = data.Song('Kanye','Runaway',data.Duration(3,51))
+        song2 = data.Song('Travis Scott','FEIN',data.Duration(4,51))
+        song3 = data.Song('SZA','Kill Bill',data.Duration(3,31))
+        song4 = data.Song('NewJeans','OMG',data.Duration(4,50))
+        song_list=[song1,song2,song3,song4]
+        nums_list=[0,1,2,3,2,3]
+        expected=data.Duration(25,24)
+        self.assertEqual(expected, hw2.running_time(song_list,nums_list))
 
+    def test_running_time_2(self):
+        song1 = data.Song('Kanye','Runaway',data.Duration(3,49))
+        song2 = data.Song('Travis Scott','FEIN',data.Duration(4,51))
+        song3 = data.Song('SZA','Kill Bill',data.Duration(5,21))
+        song4 = data.Song('NewJeans','OMG',data.Duration(4,50))
+        song_list=[song1,song2,song3,song4]
+        nums_list=[0,1,2,3,2,3]
+        expected=data.Duration(29,2)
+        self.assertEqual(expected, hw2.running_time(song_list,nums_list))
 
     # Part 5
+    def test_validate_route(self):
+        city_links = [['san luis obispo', 'santa margarita'],
+            ['san luis obispo', 'pismo beach'],
+            ['atascadero', 'santa margarita'],
+            ['atascadero', 'creston']]
+        route=['san luis obispo', 'santa margarita', 'atascadero']
+        self.assertEqual(True, hw2.validate_route(city_links,route))
 
+    def test_validate_route_2(self):
+        city_links = [['san luis obispo', 'santa margarita'],
+            ['san luis obispo', 'pismo beach'],
+            #['atascadero', 'santa margarita'],
+            ['atascadero', 'creston']]
+        route=['san luis obispo', 'santa margarita', 'atascadero']
+        self.assertEqual(False, hw2.validate_route(city_links,route))
 
     # Part 6
+    def test_longest_repetition(self):
+        num_list=[1, 1, 2, 2, 1, 1, 1, 3]
+        expected=4
+        self.assertEqual(expected, hw2.longest_repetition(num_list))
 
-
-
-
+    def test_longest_repetition_2(self):
+        num_list=[1, 1, 1, 2, 2, 1, 1, 1, 3,3,3,3]
+        expected=8
+        self.assertEqual(expected, hw2.longest_repetition(num_list))
 
 if __name__ == '__main__':
     unittest.main()
